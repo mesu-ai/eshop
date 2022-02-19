c50e264d2941f9f54a313a75166666898eca5ee0
mesu-ai <mesu.s.ss@gmail.com>
2022-02-19 11:53:00 +0600

    update category

--- src/pages/home/categories/card/CategoryCard.js
+++ src/pages/home/categories/card/CategoryCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../Category.css';
 import { CardActionArea, Grid } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -11,15 +12,16 @@ const CategoryCard = ({category}) => {
       <Grid item xs={2} sm={4} md={4}>
       <Card sx={{  }}>
       <CardActionArea>
-        <CardMedia
+        {/* <CardMedia className='category-content'
           sx={{mx:'auto',py:2}}
           component="img"
           style={{height:'150px',width:'280px'}}
           image={image}
           alt="green iguana"
-        />
+        /> */}
+        <img className='category-content' src={image} alt="Category" />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+          <Typography sx={{fontSize:{xs:'4vw', md:'2vw',lg:'1.8vw'}}}  gutterBottom variant="h5" component="div">
            {name}
           </Typography>
           <Typography variant="body2" color="text.secondary">
